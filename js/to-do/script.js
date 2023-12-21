a67e688a5b6fca568a330a9988ad09340928eecf
samnart <tettey1234567@gmail.com>
2023-12-21 17:03:38 +0100

    simple todo web-app

---
+++ js/to-do/script.js
@@ -0,0 +1,20 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText != '') {
+        const taskItem = document.createElement('li');
+        taskItem.textContent = taskText;
+
+        taskList.appendChild(taskItem);
+
+        taskInput.value = '';
+    } else {
+        alert('Please enter a valid task!');
+    }
+}
