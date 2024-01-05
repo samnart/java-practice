728cd88201da36521ccde4dcabe2436908913436
samnart <tettey1234567@gmail.com>
2024-01-05 14:25:29 +0100

    findLargestNumber.js

---
+++ js/tiny-codes/findLargestNumber.js
@@ -0,0 +1,20 @@
+function findLargestNumber(numbers) {
+
+    let largest = numbers[0];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (largest < numbers[i]) {
+            largest = numbers[i];
+        }
+    } 
+
+    return largest;
+
+}
+
+console.log(findLargestNumber([23, 45, 6]));              // Should return 45
+console.log(findLargestNumber([5, 10, 2, 8]));            // Should return 10
+console.log(findLargestNumber([15, 3, 7, 22, 45]));       // Should return 45
+console.log(findLargestNumber([-5, -10, -2, -8]));        // Should return -2
+
+return Math.max(...numbers);
