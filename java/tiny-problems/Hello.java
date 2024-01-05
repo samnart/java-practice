2a83744e650557833a9adbbbbc7c26d0244b1d15
samnart <tettey1234567@gmail.com>
2024-01-05 15:19:53 +0100

    Hello.java

---
+++ java/tiny-problems/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * This is a simple Java program that prints "Hello, World!" to the console.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
