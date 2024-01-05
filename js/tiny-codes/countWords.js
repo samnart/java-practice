7cbcfd684a2a769dde7b7cee5c3d9a5522547e33
samnart <tettey1234567@gmail.com>
2024-01-05 15:13:15 +0100

    countWords.js

---
+++ js/tiny-codes/countWords.js
@@ -0,0 +1,9 @@
+function countWords(sentence) {
+    let words = sentence.split(/\s+/);
+    let wordCount = words.length;
+    return wordCount;
+}
+
+console.log(countWords("Hello world!"));  // Should return 2
+console.log(countWords("This is a sample sentence."));  // Should return 5
+console.log(countWords("  Extra   spaces  should not affect the count.   "));
