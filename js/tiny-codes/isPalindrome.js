5a1b3e976d1632614db2fa03b010652e992bc5f9
samnart <tettey1234567@gmail.com>
2024-01-05 13:37:29 +0100

    isPalindrome

--- js/tiny-codes/isPalindrome.js
+++ js/tiny-codes/isPalindrome.js
@@ -1,4 +1,19 @@
 function isPalindrome(str) {
-    let phrase = str.toLowerCase();
-    phrase = phrase.replace(/\s/g, '');
+
+    const lowerStr = str.toLowerCase();
+    const newStr = lowerStr.replace(/[^a-z0-9]/g, '');
+
+    let start = 0;
+    let end = newStr.length - 1;
+
+    while(start < end) {
+        if (newStr[start] != newStr[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 }
+
+console.log(isPalindrome("A man, a plan, a canal Panama"));
