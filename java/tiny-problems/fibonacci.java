34db58dfcfff5db9584beda1ad09eb7768179520
samnart <tettey1234567@gmail.com>
2024-01-07 13:51:27 +0100

    fibonacci numbers solved

--- java/tiny-problems/fibonacci.java
+++ java/tiny-problems/fibonacci.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 public class fibonacci {
 
     public static void printFibonacci(int N) {
-
+        int prev = 0;
+        int current = 1;
+        for (int i = 0; i < N; i++) {
+            System.out.print(prev + " ");
+            int next = prev + current;
+            prev = current;
+            current = next;
+        }
     }
 
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter the number fo Fibonacci numbers to print: ");
             int N = scanner.nextInt();
-            
+
+            printFibonacci(N);
         }
     }
 }
