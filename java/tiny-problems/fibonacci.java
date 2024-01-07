9445bf52fbbb96bd96e6c475d72467db13828e40
samnart <tettey1234567@gmail.com>
2024-01-07 13:08:56 +0100

    touch hello.java

---
+++ java/tiny-problems/fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+    public static void printFibonacci(int N) {
+
+    }
+
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter the number fo Fibonacci numbers to print: ");
+            int N = scanner.nextInt();
+            
+        }
+    }
+}
