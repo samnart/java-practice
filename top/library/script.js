fc221ccdeee7f2c67841382a47226222627d1948
samnart <tettey1234567@gmail.com>
2024-01-11 14:32:19 +0100

    created the js part of the library

--- top/library/script.js
+++ top/library/script.js
@@ -1,28 +1,21 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Books(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read} yet`;
-}
-
-
-function addBookToLibrary() {
-    const title = prompt('Tittle of the book: ');
-    const author = prompt('Who is the author: ');
-    const pages = prompt('How many pages does the book have: ');
-    const read = prompt('Read or Not Read: ');
-
-    const newBook = new Book(title, author, pages, read);
-    
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Books(title, author, pages, read);
     myLibrary.unshift(newBook);
-
-    console.log(myLibrary);
+    displayBooks();
 }
 
-addBookToLibrary();
+function displayBooks() {
+    const bookShelf = document.getElementById("bookShelf");
+    bookShelf.innerHTML = "";
+
+
+}
