7e3a02d040fee330cf1153ce1aa5928543b33a89
samnart <tettey1234567@gmail.com>
2024-02-13 16:23:43 +0100

    Done with core java for the day

--- complete/Hello.java
+++ complete/Hello.java
@@ -3,7 +3,12 @@ class Hello {
 
     public static void main(String[] args) {
         
-        
+        try {
+            Class.forName("yello");
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 
