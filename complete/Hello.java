3591094988292ebf25eb863bcd7e7b65565bbd17
samnart <tettey1234567@gmail.com>
2024-02-14 11:17:53 +0100

    core java from yesterday

--- complete/Hello.java
+++ complete/Hello.java
@@ -1,15 +1,44 @@
+class z{}
+
+class A implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i ++) {
+            System.out.println("hi");
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i ++) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 
 class Hello {
 
     public static void main(String[] args) {
         
-        try {
-            Class.forName("yello");
-        } 
-        catch (Exception e) {
-            System.out.println(e);
-        }
+        Runnable obj1 = new A();
+        Runnable obj2 = new B();
 
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
     }
 
 }
