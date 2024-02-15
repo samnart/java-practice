cb3456effb0c4af035e801114f5c7aa8ce7f1b5f
samnart <tettey1234567@gmail.com>
2024-02-15 12:12:02 +0100

    dsa java

--- complete/adt/adt/src/Demo.java
+++ complete/adt/adt/src/Demo.java
@@ -1,9 +1,179 @@
+// LINKED LIST
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MERGE SORT
+
+//public class Demo {
+//
+//    private static void mergeSort(int[] arr, int left, int right) {
+//
+//        if( left < right) {
+//            int mid = (left + right) / 2;
+//            mergeSort(arr, left, mid);
+//            mergeSort(arr, mid + 1, right);
+//
+//            merge(arr, left, mid, right);
+//        }
+//
+//    }
+//
+//    private static void merge(int[] arr, int left, int mid, int right) {
+//
+//        int n1 = mid - left + 1;
+//        int n2 = right - mid;
+//
+//        int leftArr[] = new int[n1];
+//        int rightArr[] = new int[n2];
+//
+//        for (int x = 0; x < n1; x++) {
+//            leftArr[x] = arr[left + x];
+//        }
+//
+//        for (int x = 0; x < n2; x++) {
+//            rightArr[x] = arr[mid + 1 + x];
+//        }
+//
+//        int i = 0;
+//        int j = 0;
+//        int k = left;
+//
+//        while (i < n1 && j < n2) {
+//
+//            if (leftArr[i] <= rightArr[j]) {
+//                arr[k] = leftArr[i];
+//                i++;
+//            }
+//            else {
+//                arr[k] = rightArr[j];
+//                j++;
+//            }
+//            k++;
+//        }
+//
+//        while (i < n1) {
+//            arr[k] = leftArr[i];
+//            i++;
+//            k++;
+//        }
+//
+//        while (j < n2) {
+//            arr[k] = rightArr[j];
+//            j++;
+//            k++;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int arr[] = {8, 5, 9, 1, 6, 7, 7, 7, 10897, 2334, 914, 932847, 928};
+//
+//        System.out.println("Before MERGER sort: ");
+//        for (int n : arr) {
+//            System.out.print(n + " ");
+//        }
+//
+//
+//        mergeSort(arr, 0, arr.length - 1);
+//
+//        System.out.println();
+//        System.out.println("After MERGE sort: ");
+//        for (int n : arr) {
+//            System.out.print(n + " ");
+//        }
+//    }
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // QUICK SORT
 
-public class Demo {
-    public static void main(String[] args) {}
-}
+//public class Demo {
+//    public static void main(String[] args) {
+//
+//        int[] arr = {5, 62, 2, 3, 111, 81, 4};
+//
+//        quickSort(arr, 0, arr.length - 1);
+//
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+//    }
+//
+//    public static void quickSort(int[] arr, int low, int high) {
+//        if (low < high) {
+//
+//            int pi = partition(arr, low, high);
+//
+//            quickSort(arr, low, pi - 1);
+//            quickSort(arr, pi + 1, high);
+//        }
+//    }
+//
+//    private static int partition(int[] arr, int low, int high) {
+//        int pivot = arr[high];
+//        int i = low - 1;
+//
+//        for (int j = low; j < high; j++) {
+//            if (arr[j] < pivot) {
+//                i++;
+//                int temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//        int temp = arr[i + 1];
+//        arr[i + 1] = arr[high];
+//        arr[high] = temp;
+//
+//        return i + 1;
+//    }
+//}
 
 
 
---
+++ complete/dsa/Runner.java
@@ -0,0 +1,13 @@
+// import java.util.LinkedList;
+
+public class Runner {
+    public static void main (String[] args) {
+
+        LinkedList list = new LinkedList();
+        list.insert(18);
+        list.insert(45);
+        list.insert(12);
+
+        list.show();
+    }
+}
